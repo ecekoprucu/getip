4205d9efed4fc3b833ddd2442d80b12715d5a13e
Medmaksi <ecekoprucu@gmail.com>
2021-11-24 17:35:59 +0300

    test_ipo

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+function Home({ip}) {
+console.log(ip)
   return (
     <div className={styles.container}>
       <Head>
@@ -67,3 +68,9 @@ export default function Home() {
     </div>
   )
 }
+Home.getInitialProps = async ({ req }) => {
+  const ip = req.headers["x-real-ip"] || req.connection.remoteAddress;
+  return { ip };
+};
+
+export default Home;
